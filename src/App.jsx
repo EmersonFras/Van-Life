60fe2860eb53ae4b6fd23847141da2719fe115fe
emersonfras <emersonfrasure@gmail.com>
2025-01-22 18:50:06 -0500

    Adds About and Home page
    Most CSS is provided through Scrimba Course. Minor Personal changes to the css for hero styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+        <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+        </header>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
     </>
   )
 }
